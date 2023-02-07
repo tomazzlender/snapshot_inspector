17e6f705177e321c2923c02c1c07cd923fafecee
Tomaz Zlender <tomaz@zlender.se>
2023-02-07 21:34:23 +0100

    Wrap all class methods in a self block

--- app/models/minitest/snapshot/snapshot.rb
+++ app/models/minitest/snapshot/snapshot.rb
@@ -1,29 +1,31 @@
 module Minitest
   module Snapshot
     class Snapshot
-      def self.all
-        source_directory = Rails.root.join("tmp/snapshots")
-        test_case_files = Dir.glob("#{source_directory}/**/*.{json}")
+      class << self
+        def all
+          source_directory = Rails.root.join("tmp/snapshots")
+          test_case_files = Dir.glob("#{source_directory}/**/*.{json}")
 
-        test_case_files
-          .map { |test_case_file| JSON.parse(File.read(test_case_file), symbolize_names: true) }
-          .flatten
-          .sort_by { |snapshot| snapshot[:created_at] }
-          .reverse
-      end
-
-      def self.grouped_by_test_class
-        all.group_by do |snapshot|
-          snapshot[:test_class]
+          test_case_files
+            .map { |test_case_file| JSON.parse(File.read(test_case_file), symbolize_names: true) }
+            .flatten
+            .sort_by { |snapshot| snapshot[:created_at] }
+            .reverse
         end
-      end
 
-      def self.find(slug)
-        file_path = slug.split("/")[0..-2].join("/")
-        absolute_file_path = Rails.root.join("tmp/snapshots/#{file_path}.json")
-        snapshots = JSON.parse(File.read(absolute_file_path), symbolize_names: true)
+        def grouped_by_test_class
+          all.group_by do |snapshot|
+            snapshot[:test_class]
+          end
+        end
 
-        snapshots.find { |snapshot| snapshot[:slug] == slug }
+        def find(slug)
+          file_path = slug.split("/")[0..-2].join("/")
+          absolute_file_path = Rails.root.join("tmp/snapshots/#{file_path}.json")
+          snapshots = JSON.parse(File.read(absolute_file_path), symbolize_names: true)
+
+          snapshots.find { |snapshot| snapshot[:slug] == slug }
+        end
       end
     end
   end
