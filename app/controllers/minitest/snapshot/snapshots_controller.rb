1f63346a980d89e314ed220a7ba418c699fe76fb
Tomaz Zlender <tomaz@zlender.se>
2023-02-07 18:55:51 +0100

    List all snapshots on `snapshots#index` page with most recent on top

--- app/controllers/minitest/snapshot/snapshots_controller.rb
+++ app/controllers/minitest/snapshot/snapshots_controller.rb
@@ -1,6 +1,7 @@
 module Minitest::Snapshot
   class SnapshotsController < ApplicationController
     def index
+      @snapshots = Snapshot.all
     end
   end
 end
--- test/controllers/minitest/snapshot/snapshots_controller_test.rb
+++ test/controllers/minitest/snapshot/snapshots_controller_test.rb
@@ -3,10 +3,22 @@ require "test_helper"
 module Minitest::Snapshot
   class SnapshotsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
+    include Test::IntegrationHelpers
+
+    setup do
+      Rails.root.join("tmp", "snapshots").rmtree
+    end
 
     test "should get index" do
+      destination = Rails.root.join("tmp/snapshots/minitest/snapshot/snapshots_controller_test/")
+      destination.mkpath
+      FileUtils.copy(file_fixture("test_should_get_index.json"), destination)
+
       get root_path
+
       assert_response :success
+      assert_select "h1", text: "Snapshots"
+      assert_select "li", text: /Should get index/
     end
   end
 end
