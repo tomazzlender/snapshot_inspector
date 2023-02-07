a2f047e911357f870983cb2a28ebf705c8b24b90
Tomaz Zlender <tomaz@zlender.se>
2023-02-07 17:38:24 +0100

    Save details about a response when a snapshot is taken
    - when the snapshot was taken
    - the response body
    - test case name from which the snapshot was taken
    - test case class in which the test case was run
    
    Save the above information in a JSON file inside a `tmp/snapshots` folder.
    
    Allow taking multiple snapshots per single test case.

--- lib/minitest/snapshot/test/integration_helpers.rb
+++ lib/minitest/snapshot/test/integration_helpers.rb
@@ -11,8 +11,28 @@ module Minitest
         # more snapshots in a single test case.
         #
         # The method implementation is a work in progress.
-        def take_snapshot(_response)
-          puts "Taken snapshot: #{self.class.to_s.underscore}/#{method_name}"
+        def take_snapshot(response)
+          test_case_name = method_name
+          test_class_name = self.class.to_s
+
+          file_path = Rails.root.join("tmp", "snapshots", test_class_name.underscore, "#{method_name}.json")
+          file_path.dirname.mkpath
+          file_path.write([].to_json) unless File.exist?(file_path)
+
+          file_content = File.read(file_path)
+          file_json = JSON.parse(file_content)
+
+          snapshot = {
+            created_at: Time.current,
+            response_body: response.body,
+            test_case_name: test_case_name,
+            test_case_human_name: test_case_name.gsub(/^test_/, "").humanize,
+            test_class: test_class_name
+          }
+
+          file_json << snapshot
+
+          file_path.write(JSON.pretty_generate(file_json))
         end
       end
     end
