785ae78764892bc1acb91e87cd7b2ffe878f3c0e
Tomaz Zlender <tomaz@zlender.se>
2023-02-08 01:11:53 +0100

    Autoload test integration helpers
    The change eliminates the last manual step we needed to make after installing a gem.
    
    All we need to do now is to add the gem to the Gemfile.
    
    After we can immediately start using #take_snapshot method in the integration tests.

--- lib/minitest/snapshot/engine.rb
+++ lib/minitest/snapshot/engine.rb
@@ -3,6 +3,12 @@ module Minitest
     class Engine < ::Rails::Engine
       isolate_namespace Minitest::Snapshot
 
+      initializer "minitest_snapshot.include_test_integration_helpers" do |_app|
+        ActiveSupport.on_load(:action_dispatch_integration_test) do
+          include Minitest::Snapshot::Test::IntegrationHelpers
+        end
+      end
+
       config.after_initialize do |app|
         app.routes.prepend do
           mount Minitest::Snapshot::Engine, at: "/rails/snapshots" if Rails.env.development?
