d99b9672aa363efc9058c9905571df25b1986f0b
Tomaz Zlender <tomaz@zlender.se>
2023-02-08 06:43:40 +0100

    Autoload routes after initialize
    It reduces the number of steps we needs to make to start using the engine.
    
    We use /rails/* prefix in the URL path since it ensures there will be no clashes with other 3rd party libraries.
    
    It only mounts the routes in development environment. We don't need them in test environment since a webserver isn't running in that time.

--- lib/minitest/snapshot/engine.rb
+++ lib/minitest/snapshot/engine.rb
@@ -2,6 +2,12 @@ module Minitest
   module Snapshot
     class Engine < ::Rails::Engine
       isolate_namespace Minitest::Snapshot
+
+      config.after_initialize do |app|
+        app.routes.prepend do
+          mount Minitest::Snapshot::Engine, at: "/rails/snapshots" if Rails.env.development?
+        end
+      end
     end
   end
 end
