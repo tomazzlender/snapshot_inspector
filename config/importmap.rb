30cba1078e94f783fbea35d177830e8f935a513e
Tomaz Zlender <tomaz@zlender.se>
2023-02-08 19:04:06 +0100

    Pin stimulus-use and hotkeys-js libraries
    We want to add keyboard shortcuts to the snapshots#index page.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,8 @@ require "minitest/snapshot"
 Minitest::Snapshot.configuration.importmap.draw do
   pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
   pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+  pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js"
+  pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.10.1/dist/hotkeys.esm.js"
 
   pin "application", to: "minitest/snapshot/application.js", preload: true
 
