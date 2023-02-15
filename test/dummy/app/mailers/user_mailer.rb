bb0ea46b2768ff4fde5ce068f9bc6c90cde8de2e
Tomaz Zlender <tomaz@zlender.se>
2023-02-15 23:30:02 +0100

    Add three variations of mailers - html and text, html only and text only
    Support for displaying all mailer variations is a bit rough around the edges at the moment.
    
    More needs to be done.
    
    The view code is mostly copy paste from rails' official preview email.html.erb template.
    
    I want to mimic the experience to make it familiar and make the migration path easier.

--- test/dummy/app/mailers/user_mailer.rb
+++ test/dummy/app/mailers/user_mailer.rb
@@ -7,6 +7,28 @@ class UserMailer < ApplicationMailer
       to: email_address_with_name(@recepient.email, @recepient.name),
       bcc: ["bcc@example.com"],
       subject: "Welcome!"
+    ) do |format|
+      format.html
+      format.text
+    end
+  end
+
+  def reminder(recipient)
+    @recepient = recipient
+    attachments["mail_attachment.txt"] = ViewInspector::Engine.root.join("test/fixtures/files/mail_attachment.txt").read
+    mail(
+      to: email_address_with_name(@recepient.email, @recepient.name),
+      subject: "Remember to take care of..."
     )
+  end
+
+  def plaintext(recipient)
+    @recepient = recipient
+    mail(
+      to: email_address_with_name(@recepient.email, @recepient.name),
+      subject: "Plain text is back"
+    ) do |format|
+      format.text
+    end
   end
 end
--- test/dummy/test/mailers/user_mailer_test.rb
+++ test/dummy/test/mailers/user_mailer_test.rb
@@ -9,13 +9,43 @@ class UserMailerTest < ActionMailer::TestCase
       take_snapshot mail
     end
 
-    expected_mail = to_mail(file_fixture("user_mailer_test/test_welcome_0.json"))
     persisted_mail = to_mail(ViewInspector::Storage.processing_directory.join("user_mailer_test/test_welcome_0.json"))
-    assert_equal expected_mail.body.decoded, persisted_mail.body.decoded
 
     assert_equal "Welcome!", persisted_mail.subject
     assert_equal [recepient.email], persisted_mail.to
     assert_equal ["bcc@example.com"], persisted_mail.bcc
+    assert_equal ["no-reply@example.com"], persisted_mail.from
+  end
+
+  test "reminder" do
+    recepient = Struct.new(:name, :email).new("John", "john@example.com")
+    mail = UserMailer.reminder(recepient)
+
+    ViewInspector.configuration.stub(:snapshot_taking_enabled, true) do
+      take_snapshot mail
+    end
+
+    persisted_mail = to_mail(ViewInspector::Storage.processing_directory.join("user_mailer_test/test_reminder_0.json"))
+
+    assert_equal "Remember to take care of...", persisted_mail.subject
+    assert_equal [recepient.email], persisted_mail.to
+    assert_nil persisted_mail.bcc
+    assert_equal ["no-reply@example.com"], persisted_mail.from
+  end
+
+  test "plaintext" do
+    recepient = Struct.new(:name, :email).new("John", "john@example.com")
+    mail = UserMailer.plaintext(recepient)
+
+    ViewInspector.configuration.stub(:snapshot_taking_enabled, true) do
+      take_snapshot mail
+    end
+
+    persisted_mail = to_mail(ViewInspector::Storage.processing_directory.join("user_mailer_test/test_plaintext_0.json"))
+
+    assert_equal "Plain text is back", persisted_mail.subject
+    assert_equal [recepient.email], persisted_mail.to
+    assert_nil persisted_mail.bcc
     assert_equal ["no-reply@example.com"], persisted_mail.from
   end
 
